139694335225c314017cf75bba9f55d638c504d9
Inês Major <98774210+inesnmajor@users.noreply.github.com>
2025-04-09 11:57:07 +0100

    first creation of final notebook and library

--- library.py
+++ library.py
@@ -1,7 +1,7 @@
-import matplotlib as plt
-import numpy as np
-
 def vis_images(train):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
     plt.figure(figsize=(10, 10))
     for images, labels in train.take(1):
         for i in range(9):
@@ -9,3 +9,4 @@ def vis_images(train):
             plt.imshow(np.array(images[i]).astype("uint8"))
             plt.title(int(np.argmax(labels[i])))
             plt.axis("off")
+    plt.show()
